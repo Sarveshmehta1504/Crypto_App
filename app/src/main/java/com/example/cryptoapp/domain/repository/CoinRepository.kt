d624361cd7e1589df27ec24e27e1a1690cdd1f66
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2024-04-21 03:45:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cryptoapp/domain/repository/CoinRepository.kt
@@ -0,0 +1,12 @@
+package com.example.cryptoapp.domain.repository
+
+import com.example.cryptoapp.data.remote.dto.CoinDetailDto
+import com.example.cryptoapp.data.remote.dto.CoinDto
+import com.example.cryptoapp.domain.model.Coin
+
+interface CoinRepository {
+    suspend fun getCoins(): List<CoinDto>
+
+    suspend fun getCoinById(coinId: String): CoinDetailDto
+
+}
